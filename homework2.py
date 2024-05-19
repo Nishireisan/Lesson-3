65e5b655b93faadad1021438e667074ade6a6fe4
Nishireisan <andrey240300@gmail.com>
2024-05-19 18:03:56 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = 'Andrei'
+print('Name: ' + name)
+age = 24
+print('Age: ' + str(age))
+new_age = age + 1
+print('New Age: ' + str(new_age))
+is_student = True
+print('Is student: ' + str(is_student))
